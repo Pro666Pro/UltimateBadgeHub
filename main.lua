064cb925fcd2850952c21da72f1acdfaf6ff52c4
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-02 21:34:08 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/BypassAntiCheat/main/main.lua'))()
 
+print("Ultimate Badge Hub Started")
+
 loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/45ff97383d7be43009d0aa3f0c7b7469/raw/d3dfb5b9c2fb349db0de560f40932d027c41978f/main.lua"))()
