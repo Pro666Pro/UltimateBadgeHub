d77e557959ef72b928794311360160c3ea6b0cdb
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-02 21:14:39 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/BypassAntiCheat/main/main.lua'))()
 
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/bcf8a08b86a2c4c9f3441ed70edc7691/raw/0c7b37925022f98acede615c78970305eae0659c/main.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/73aaf291bfc32831f54a843ee29ed638/raw/2a61eca1255c2faf81e520f444e29646213a0036/main.lua"))()
