a5908fc9e1456645851ad3c0167cdbf01c36c5be
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-03 21:44:35 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/BypassAntiCheat/main/main.lua'))()
 
+print("Ultimate Badge Hub Started")
+
 local GameName = "Ultimate Badge Hub Launcher ✨"
 
-local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pro666Pro/DraggableOrionLib/main/main.lua")))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({IntroText = "UBH v3 LAUNCHER", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 local Launcher = Window:MakeTab({
@@ -13,28 +15,26 @@ local Launcher = Window:MakeTab({
 
 Launcher:AddParagraph("Choose Localization", "Default = English")
 
-Launcher:AddButton({
-	Name = "English Language",
-	Callback = function()
 _G.Localization = "English"
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Success!",Text = "Chosen localization = English" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
-	 end
-})
-
-Launcher:AddButton({
-	Name = "Русский язык",
-	Callback = function()
+Launcher:AddDropdown({
+	Name = "Choose Localization",
+	Default = "English",
+	Options = {"English", "Русский", "Español"},
+	Callback = function(Value)
+if Value == "English" then
+wait(0.01)
+_G.Localization = "English"
+wait(0.01)
+elseif Value == "Русский" then
+wait(0.01)
 _G.Localization = "Русский"
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Success!",Text = "Chosen localization = Russian" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
-	 end
-})
-
-Launcher:AddButton({
-	Name = "Idioma español",
-	Callback = function()
+wait(0.01)
+elseif Value == "Español" then
+wait(0.01)
 _G.Localization == "Español"
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Success!",Text = "Chosen localization = Spanish" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
-	 end
+wait(0.01)
+end
+	end    
 })
 
 Launcher:AddParagraph("Launch Script", "Edit setting above if you want to, and then launch script")
@@ -43,7 +43,7 @@ Launcher:AddButton({
 	Name = "Launch Ultimate Badge Hub",
 	Callback = function()
 OrionLib:Destroy()
-wait(2)
+wait(3)
 loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/c13f44444bd20a832dff2210740d9e1a/raw/69b9eba96decf12ecd64cda70d7f25c03d76d959/main.lua"))()
 	 end
 })
