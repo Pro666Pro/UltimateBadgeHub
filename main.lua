e6d13f92e8e8d70234f7599434dbda0d736cf6ba
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-03 22:05:42 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,46 +2,48 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/BypassAntiC
 
 print("Ultimate Badge Hub Started")
 
-local GameName = "Ultimate Badge Hub Launcher"
+local GameName = "Ultimate Badge Hub Launcher ✨"
 
 local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pro666Pro/DraggableOrionLib/main/main.lua")))()
-local Window = OrionLib:MakeWindow({IntroText = "UBH LAUNCHER", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "UBH v3 LAUNCHER", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
-local Badge = Window:MakeTab({
-	Name = "Settings",
+local Launcher = Window:MakeTab({
+	Name = "Setup",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
-Badge:AddButton({
-	Name = "Launch English UBH",
-	Callback = function()
+Launcher:AddParagraph("Choose Localization", "Default = English")
+
 _G.Localization = "English"
-wait(0.1)
-OrionLib:Destroy()
-wait(1)
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/c13f44444bd20a832dff2210740d9e1a/raw/69b9eba96decf12ecd64cda70d7f25c03d76d959/main.lua"))()
-	 end
-})
-
-Badge:AddButton({
-	Name = "Запустить Русский UBH",
-	Callback = function()
+Launcher:AddDropdown({
+	Name = "Choose Localization",
+	Default = "English",
+	Options = {"English", "Русский", "Español"},
+	Callback = function(Value)
+if Value == "English" then
+wait(0.01)
+_G.Localization = "English"
+wait(0.01)
+elseif Value == "Русский" then
+wait(0.01)
 _G.Localization = "Русский"
-wait(0.1)
-OrionLib:Destroy()
-wait(1)
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/c13f44444bd20a832dff2210740d9e1a/raw/69b9eba96decf12ecd64cda70d7f25c03d76d959/main.lua"))()
-	 end
+wait(0.01)
+elseif Value == "Español" then
+wait(0.01)
+_G.Localization = "Español"
+wait(0.01)
+end
+	end    
 })
 
-Badge:AddButton({
-	Name = "Lanzamiento UBH Española",
+Launcher:AddParagraph("Launch Script", "Edit setting above if you want to, and then launch script")
+
+Launcher:AddButton({
+	Name = "Launch Ultimate Badge Hub",
 	Callback = function()
-_G.Localization == "Español"
-wait(0.1)
 OrionLib:Destroy()
-wait(1)
+wait(2)
 loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/c13f44444bd20a832dff2210740d9e1a/raw/69b9eba96decf12ecd64cda70d7f25c03d76d959/main.lua"))()
 	 end
 })
