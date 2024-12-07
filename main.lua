9f8158bbc6d4243b99171f475f1a652cb47ec09f
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-07 15:17:16 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -40,13 +40,18 @@ end
 Launcher:AddParagraph("Choose Version", "Default = Latest Version")
 
 Launcher:AddParagraph("WARNING!", "Versions that lower than 3.2, don't support localizations!")
+Launcher:AddParagraph("WARNING!", "Versions that higher than 3.3, don't support spanish localization!")
 
 Launcher:AddDropdown({
 	Name = "Choose Version",
-	Default = "v3.3",
-	Options = {"v3.3", "v3.2", "v3.1", "v3.0"},
+	Default = "v3.4",
+	Options = {"v3.4", "v3.3", "v3.2", "v3.1", "v3.0"},
 	Callback = function(Value)
-if Value == "v3.3" then
+if Value == "v3.4" then
+wait(0.01)
+_G.Version = "v3.4"
+wait(0.01)
+elseif Value == "v3.3" then
 wait(0.01)
 _G.Version = "v3.3"
 wait(0.01)
@@ -71,7 +76,11 @@ Launcher:AddParagraph("Launch Script", "Edit settings above if you want to, and 
 Launcher:AddButton({
 	Name = "Launch Ultimate Badge Hub",
 	Callback = function()
-if _G.Version == "v3.3" then
+if _G.Version == "v3.4" then
+OrionLib:Destroy()
+wait(0.5)
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/17ad21691c1d4ab925ebe505e5a3f5bb/raw/88a23e5c7300a4d1e8a92b9d15e837a7bba61bad/main.lua"))()
+elseif _G.Version == "v3.3" then
 OrionLib:Destroy()
 wait(0.5)
 loadstring(game:HttpGet("https://gist.githubusercontent.com/Pro666Pro/7867d21551ffc0bd8a00585e81453cbd/raw/5a129ad08b07b5965535541a1e7fcbfb9fbbe0a1/main.lua"))()
