0dd19611b88f73f3d89bab319679d3ca2b2ec7bf
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2026-07-08 05:01:47 +0900

    Typo fix

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+-- I misstyped. It was supposed to be 8th July
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Maintenance",
 	Text = "This script is currently updating. This script will be back before July 8th, 1:00 AM (KST). Thank you for using this script!",
