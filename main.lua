6cc4e39aa28cf84477006f16b3012dc5748c3156
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2026-07-08 05:00:59 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Maintenance",
-	Text = "This script is currently updating. This script will be back before July 7th, 1:00 AM (KST). Thank you for using this script!",
+	Text = "This script is currently updating. This script will be back before July 8th, 1:00 AM (KST). Thank you for using this script!",
     Icon = "rbxassetid://101714902106848",
 	Duration = 36000,
 	Button1 = "Confirm"
