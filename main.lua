451260a4fd0c7281769a5de65e9eb192d5147355
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2026-07-09 01:28:34 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,1 @@
--- I misstyped. It was supposed to be 8th July
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Maintenance",
-	Text = "This script is currently updating. This script will be back before July 8th, 1:00 AM (KST). Thank you for using this script!",
-    Icon = "rbxassetid://101714902106848",
-	Duration = 36000,
-	Button1 = "Confirm"
-})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NewNexer/NexerHub/refs/heads/main/SB/UBH/Latest.lua"))()
